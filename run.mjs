3ca4fb913909db26b499b063cce7b43e347792b2
Gregor Wolf <gregor.wolf@gmail.com>
2021-12-05 09:21:11 +0100

    prettier

--- run.mjs
+++ run.mjs
@@ -5,7 +5,9 @@ async function run() {
   const day = process.argv[2];
   console.log("# Day " + day);
 
-  const files = fs.readdirSync("output").filter(n => n.includes("day"+day) && n.endsWith(".clas.js"));
+  const files = fs
+    .readdirSync("output")
+    .filter((n) => n.includes("day" + day) && n.endsWith(".clas.js"));
   if (files.length !== 1) {
     throw "Class not found for this day";
   }
@@ -19,24 +21,28 @@ async function run() {
 
   const className = Object.keys(clas)[0];
   const instance = new clas[className]();
-  const methodName = Object.getOwnPropertyNames(clas[className].prototype).filter(m => m.endsWith("$solve"))[0];
+  const methodName = Object.getOwnPropertyNames(
+    clas[className].prototype
+  ).filter((m) => m.endsWith("$solve"))[0];
   console.log("Class: " + className.toUpperCase());
   console.log("Method: " + methodName.toUpperCase());
   console.log("Input: " + inputFile);
 
-  const result = await instance[methodName]( {input: input} );
+  const result = await instance[methodName]({ input: input });
   const output = abap.console.get();
   if (output && output !== "") {
     console.dir(output);
   }
 
-  console.log("\nResult:")
+  console.log("\nResult:");
   console.log(result);
 }
 
-run().then(() => {
-  process.exit();
-}).catch((err) => {
-  console.log(err);
-  process.exit(1);
-});
+run()
+  .then(() => {
+    process.exit();
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
